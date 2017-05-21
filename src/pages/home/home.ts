f7576bc93413a12f20c40c780d9fb85b28f8c46a
Kevin Mui <kmui2@wisc.edu>
2017-05-20 22:46:52 -0500

    need to add components for login and logout data fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,40 @@
 import { FirebaseProvider } from './../../providers/firebase/firebase';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
- 
+ import { Http, Response } from '@angular/http';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   // shoppingItems: FirebaseListObservable<any[]>;
-    flashcards: FirebaseListObservable<any[]>;
+    flashcards: any;
   newWord = '';
   newTranslation = '';
   user: Observable<firebase.User>;
-  constructor(public afAuth: AngularFireAuth, public navCtrl: NavController, public firebaseProvider: FirebaseProvider) {
-    this.flashcards = this.firebaseProvider.getFlashcards();
+  giphies = [];
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  selectedGif: any;
+  constructor(
+    private http: Http,
+    public afAuth: AngularFireAuth, public navCtrl: NavController, public firebaseProvider: FirebaseProvider) {
+    
+    this.firebaseProvider.getFlashcards()
+    .subscribe(flashcards =>{
+      this.flashcards = flashcards
+      for (let f of flashcards) {
+        this.performSearch(f.translation, f);
+        console.log("did search");
+      }
+      console.log("number of flahscards" + flashcards.length)
+    }
+    );
+    
+    // this.flashcards = this.firebaseProvider.getFlashcards();
   }
  
   addFlashcard() {
@@ -34,5 +51,18 @@ export class HomePage {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+  performSearch(searchTerm: string, flashcard: any): void {
+    var apiLink = this.link + searchTerm;
+
+    this.http.request(apiLink)
+      .subscribe((res: Response) => {
+        this.giphies = res.json().data;
+        console.log(this.giphies);
+        this.selectedGif = this.giphies[Math.floor(Math.random() * this.giphies.length)];
+        flashcard.gif = this.selectedGif;
+        return flashcard.gif;
+        // console.log("This is selected: " + this.selectedGif);
+      });
   }
 }
--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
- 
+
+import { Http, Response } from '@angular/http';
 @Injectable()
 export class FirebaseProvider {
- 
-  constructor(public afd: AngularFireDatabase) { }
- 
+
+  constructor(public afd: AngularFireDatabase, private http: Http) { }
+
   // getShoppingItems() {
   //   return this.afd.list('/shoppingItems/');
   // }
@@ -17,9 +18,21 @@ export class FirebaseProvider {
   // removeItem(id) {
   //   this.afd.list('/shoppingItems/').remove(id);
   // }
-  
+
+  title = 'Welcome to GiphySearch';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  giphies = [];
+  selectedGif: any;
+  flashcards: any;
   getFlashcards() {
-    return this.afd.list('/flashcards/');
+    // return this.afd.list('/flashcards/');
+    return this.afd.list('/flashcards/')
+    
+    // .subscribe((flashcards) => {
+    //   for (let f of flashcards) {
+    //     this.performSearch(f.translation, f);
+    //   }
+    // });
   }
   
   addFlashcard(word, translation) {
@@ -31,5 +44,18 @@ export class FirebaseProvider {
   }
   removeFlashcard(id) {
     this.afd.list('/flashcards/').remove(id);
+  }
+  performSearch(searchTerm: string, flashcard: any): void {
+    var apiLink = this.link + searchTerm;
+
+    this.http.request(apiLink)
+      .subscribe((res: Response) => {
+        this.giphies = res.json().data;
+        console.log(this.giphies);
+        this.selectedGif = this.giphies[Math.floor(Math.random() * this.giphies.length)];
+        flashcard.gif = this.selectedGif;
+        return flashcard.gif;
+        // console.log("This is selected: " + this.selectedGif);
+      });
   }
 }
